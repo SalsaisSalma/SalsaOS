f7c38935018dbee2ea555d65cfda0ad5558cd941
Simone Sulis <sulissimone56478r@gmail.com>
2025-05-27 13:45:32 +0100

    malloc and free now work fixed some addresses issues and did some testing to check if the two functions work, fixed some warnings

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,21 +3,45 @@
 #define VGA_BUFFER 0xB8000
 #define WHITE_ON_BLACK 0x07
 
+void cls(void) /* clears the screen */
+{
+    uint16_t blank = (WHITE_ON_BLACK << 8) | ' ';
+    uint16_t *vga = (uint16_t *)VGA_BUFFER;
+
+    for (size_t i = 0; i < 80 * 25; ++i)
+        vga[i] = blank;
+}
+
 void putchar(int pos, char c) {
-    char *video = (char*)VGA_BUFFER;
-    video[pos * 2] = c;
-    video[pos * 2 + 1] = WHITE_ON_BLACK;
+    uint16_t *vga = (uint16_t *)VGA_BUFFER;
+    vga[pos] = (WHITE_ON_BLACK << 8) | c;
 }
 
 void kernel_main() {
-    char *mem1 = malloc(16);
-    char *mem2 = malloc(16);
-    
-    
-    mem1[0] = 'A';
-    mem2[0] = 'B';
-    
-    putchar(0, mem1[0]); // Expect 'A'
-    putchar(1, mem2[0]); // Expect 'B'
-    
+
+    /* basic smoke testing for malloc and free */
+    /* 1. allocate and write */
+    cls();
+    char *m = malloc(32);
+    if (!m) { putchar(0, 'F'); return; }
+
+    const char *src = "Salsa";
+    for (size_t i = 0; src[i]; ++i) {
+        m[i] = src[i];    /* copy into heap */
+        putchar(i, m[i]); /* show it        */
+    }
+
+    /* 2. free the block and verify allocator re-uses it */
+    free(m);
+    // m = NULL;
+
+    char *a = malloc(32);         /* should give back same address */
+    if (a == m)
+        putchar(10, '+');         /* print a '+' if we got it back */
+
+    /* 3. write through the re-used block */
+    a[0] = 'O';
+    a[1] = 'K';
+    putchar(12, a[0]);
+    putchar(13, a[1]);
 }
--- src/kernel/libc/stdlib.c
+++ src/kernel/libc/stdlib.c
@@ -1,7 +1,7 @@
 #include "stdlib.h"
 
-extern uint8_t _heap_start;
-extern uint8_t _heap_end;
+extern uint8_t _heap_start[];
+extern uint8_t _heap_end[];
 
 #define MAX_PAGES 256
 
@@ -36,19 +36,19 @@ int find_free_page() {
     return -1; /* return -1 if no free page is found */    
 }
 
-uint8_t *find_valid_starting_pos(uint8_t* ptr) {
+uint8_t *find_valid_starting_pos(uint8_t* ptr, uint8_t *heap_end) {
     uint8_t is_valid; /* boolean variable */
     do {
         is_valid = 1;
         /* return false if couldn't find a valid starting position (it doesn't exist)*/
-        if (ptr >= _heap_end) {
+        if (ptr >= heap_end) {
             return NULL;
         }
         
         for (size_t i = 0; i < MAX_PAGES; i++) {
             if (!_heap_pages[i].is_free) {
                 /* break if the start of the new chunk falls into another chunk */
-                if ((ptr >= _heap_pages[i].data) && (ptr <= (uint8_t*)_heap_pages[i].data + _heap_pages[i].size)) {
+                if ((ptr >= (uint8_t*)_heap_pages[i].data) && (ptr <= (uint8_t*)(_heap_pages[i].data + _heap_pages[i].size))) {
                     is_valid = 0;
                     /* set _heap_ptr to the end of the chunk */
                     ptr = (uint8_t*)_heap_pages[i].data + _heap_pages[i].size + 8;
@@ -68,8 +68,9 @@ void *malloc(size_t size) {
         init_heap_pages();
     }
     
-    uint8_t *_heap_ptr = &_heap_start; /* start looking for free space from the start of the heap */
-    
+    uint8_t *heap_start = _heap_start;   
+    uint8_t *heap_end   = _heap_end;     
+    uint8_t *_heap_ptr  = heap_start;    // start looking for free space at the start of the heap
     /* aligning size */
     size = (size + 7) & ~7;
 
@@ -81,7 +82,7 @@ void *malloc(size_t size) {
         /* implementing a first-fit algorithm */
         do {
             /* return NULL if couldn't find a valid starting position */
-            _heap_ptr = find_valid_starting_pos(_heap_ptr);
+            _heap_ptr = find_valid_starting_pos(_heap_ptr, heap_end);
             if (!_heap_ptr) return NULL; 
             
             /* looking for a space big enough */
@@ -89,7 +90,7 @@ void *malloc(size_t size) {
             size_t end_chunk = (size_t)_heap_ptr + size; 
             
             /* return NULL if the chunk is too big to fit in the heap */
-            if (end_chunk > _heap_end) {
+            if (end_chunk > (uintptr_t)heap_end) {
                 return NULL;
             }
 
@@ -98,7 +99,7 @@ void *malloc(size_t size) {
             for (size_t i = 0; i < MAX_PAGES; i++) {
                 if (!_heap_pages[i].is_free) {
                     /* break if there is an overlap */
-                    if ((uint8_t*)_heap_pages[i].data < end_chunk &&
+                    if ((uint8_t*)_heap_pages[i].data < (uint8_t*)end_chunk &&
                         ((uint8_t*)_heap_pages[i].data + _heap_pages[i].size) > _heap_ptr) {
                         does_fit = 0;
                         /* set _heap_ptr to the end of the chunk*/
@@ -109,14 +110,14 @@ void *malloc(size_t size) {
             }
             
             /* if there is a free space allocate the memory and return ptr to memory */
-            if (does_fit && (end_chunk <= _heap_end)){
+            if (does_fit && (end_chunk <= (uintptr_t)heap_end)){
                 _heap_pages[j].size = size;
                 _heap_pages[j].data = _heap_ptr;
                 _heap_pages[j].is_free = 0;
                 return _heap_pages[j].data;
             }
             
-        } while (_heap_ptr + size < _heap_end); /* if there is enough space in the heap */
+        } while (_heap_ptr + size < heap_end); /* if there is enough space in the heap */
     }    
     /* return NULL if couldn't allocate memory */
     return NULL;
