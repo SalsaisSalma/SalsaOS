991a622a4d299883d657ad75dc11a85ed83b608c
Simone Sulis <sulissimone56478r@gmail.com>
2025-11-25 16:56:46 +0100

    defining file struct

--- src/kernel/filesystem/fs.c
+++ src/kernel/filesystem/fs.c
@@ -8,8 +8,16 @@
  */
 typedef struct fs_header {
     
-};
+} fs_header;
 
 typedef struct sector_header {
-    
+    // variables
+    void *data; // idk the maximum size until i find out how to setup this struct
+} sector_header;
+
+typedef struct file {
+    char name[10];
+    uint32_t size; // 3.99 GB max
+    uint32_t num_of_sectors;
+    sector_header start_sector;
 };
