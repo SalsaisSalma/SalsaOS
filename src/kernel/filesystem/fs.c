281657399a3c9811958892f422bfec1c2094bccd
Simone Sulis <sulissimone56478r@gmail.com>
2025-11-27 17:13:07 +0100

    maybe finding out how to do the fs

--- src/kernel/filesystem/fs.c
+++ src/kernel/filesystem/fs.c
@@ -5,25 +5,27 @@
  * having a header for every sector,
  * mapping every file name to an id
  * (it's still not clear to me how to do this)
+ * keeping it like this would mean having low ram usage
+ * but slower to find files
  */
-typedef struct fs_header {
+typedef struct fs_superblock {
     uint32_t num_of_files; // number of files
     uint32_t num_of_free_sectors; // number of available sectors
-
-} fs_header;
+} fs_superblock;
 
 typedef struct sector_header {
     uint32_t id;
     uint8_t is_free; // is sector free to use
-    void *data; // idk the maximum size until i find out how to setup this struct
+    uint64_t file_id;
+    uint32_t sector_start_file; // 0 if file starts in this sector or if sector is
+    uint8_t data[512 - 4 - 1 - 4 - 8]; // idk the maximum size until i find out how to setup this struct
 } sector_header;
 
-typedef struct file {
+typedef struct file_entry {
     char name[10]; //TODO find a fair length
     uint64_t id; //TODO define how big the file id should be
     uint32_t size; // ~4 GB max
-    uint32_t num_of_sectors; // number of sectors in which the file resides
-    sector_header start_sector; // sector id in which the file begins
+    uint32_t start_sector; // sector id in which the file begins
 };
 
 void start_fs() {
@@ -31,5 +33,5 @@ void start_fs() {
 }
 
 uint64_t file_to_id(char *name) {
-    //TODO map filename to unique id
+    //TODO map filename to unique id for faster reasearch
 }
